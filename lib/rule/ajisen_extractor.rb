628df5fe9759a8ab00444cb3a0ba1adaa6f7bdd4
allen <capken@gmail.com>
2012-12-12 12:38:07 +0800

    define page extraction rules for ajisen

---
+++ lib/rule/ajisen_extractor.rb
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+
+module Rule
+  class AjisenExtractor < Refine::HTMLExtractor
+    domain "http://www.ajisen.com.cn"
+
+    page_path :list_page, /restaurant03\.php\?cid=\d+$/i
+
+    selector :section, /<td width="205".+?>(.+?)<\/td>/mi, :list_page
+    attribute :name, "span.ajisen_heading01", :section
+    attribute :tel, /电话：(.+?)</mi, :section
+    attribute :address, /ajisen_txt03_gary">(.+?)</mi, :section
+    attribute :city, /味千-(.+)－\d+家店门店信息/mi, :section, :full_doc => true
+  end
+end
