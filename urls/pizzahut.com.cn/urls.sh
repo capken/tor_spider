57a4d6fc66a2392c346f13c4f7f1198dbd3b79a4
allen <capken@gmail.com>
2012-12-15 16:58:09 +0800

    add urls generation rules for pizzahut.com.cn

---
+++ urls/pizzahut.com.cn/urls.sh
@@ -0,0 +1,5 @@
+curl "http://www.pizzahut.com.cn/phcda/xml/GetHappyProvince.aspx" | egrep "item" |
+ruby -ne "puts [\$1, \$2].map(&:strip).join(\"\\t\") if \$_ =~ /name=\"(.+?)\" id=\"(.+?)\"/" |
+ruby -ruri -ne "pname, pid = \$_.split; puts 'http://www.pizzahut.com.cn/phcda/xml/GetHappyCity.aspx?provinceId=' + pid + '&province=' + URI.encode(pname)" |
+xargs -I URL curl URL | egrep "item" | ruby -ne "puts [\$1, \$2].join(\"\\t\") if \$_ =~ /name=\"(.+?)\" id=\"(.+?)\"/" |
+ruby -ruri -ne "cname, cid = \$_.split; puts 'http://www.pizzahut.com.cn/phcda/xml/GetHappyStore.aspx?cityid=' + cid + '&city=' + URI.encode(cname)"
