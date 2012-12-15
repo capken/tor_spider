e7f24331634b846ba554ca4b39d0965aeedc1dd3
allen <capken@gmail.com>
2012-12-15 18:53:55 +0800

    rm pizzahut urls.txt file

--- urls/pizzahut.com.cn/urls.sh
+++ urls/pizzahut.com.cn/urls.sh
@@ -3,3 +3,5 @@ ruby -ne "puts [\$1, \$2].map(&:strip).join(\"\\t\") if \$_ =~ /name=\"(.+?)\" i
 ruby -ruri -ne "pname, pid = \$_.split; puts 'http://www.pizzahut.com.cn/phcda/xml/GetHappyCity.aspx?provinceId=' + pid + '&province=' + URI.encode(pname)" |
 xargs -I URL curl URL | egrep "item" | ruby -ne "puts [\$1, \$2].join(\"\\t\") if \$_ =~ /name=\"(.+?)\" id=\"(.+?)\"/" |
 ruby -ruri -ne "cname, cid = \$_.split; puts 'http://www.pizzahut.com.cn/phcda/xml/GetHappyStore.aspx?cityid=' + cid + '&city=' + URI.encode(cname)"
+
+# details urls http://www.pizzahut.com.cn/phcda/minisite/storemap/choice.aspx?s=763
