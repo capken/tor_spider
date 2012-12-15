a586488feb47a416d77c36e05351566ee3d0f517
allen <capken@gmail.com>
2012-12-15 18:50:09 +0800

    add more attributes for pizzahut

--- lib/rule/pizzahut_extractor.rb
+++ lib/rule/pizzahut_extractor.rb
@@ -4,10 +4,12 @@ module Rule
   class PizzahutExtractor < Refine::HTMLExtractor
     domain "http://www.pizzahut.com.cn/"
 
-    page_path :list_xml, /GetHappyStore\.aspx\?cityid=\d+/i
+    page_path :list_pages, /GetHappyStore\.aspx\?cityid=\d+/i
 
-    selector :item, "//item", :list_xml
-    attribute :name, /name="(.+?)"/, :item
+    selector :item, "//item", :list_pages, :node => true
+    attribute :name, "@name", :item
+    attribute :address, "@address", :item
+    attribute :tel, "@tel", :item
 
     tag :brand_name, "必胜客"
   end
